fe2341093af95c908da6b239619970cbfc252cd0
The Legend Maker <contact@dhamodaranimmigration.com>
2026-08-23 18:09:56 +0530

    fix(transitions): remove jarring unmount exit cut for continuous, instant, fluid page transitions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -207,20 +207,17 @@ function RootComponent() {
       <div className="flex min-h-screen flex-col">
         <SiteHeader />
         <main className="flex-1 overflow-x-hidden">
-          <AnimatePresence mode="wait" initial={false}>
-            <motion.div
-              key={location.pathname}
-              initial={{ opacity: 0, y: 12 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -8 }}
-              transition={{
-                duration: 0.45,
-                ease: [0.22, 1, 0.36, 1],
-              }}
-            >
-              <Outlet />
-            </motion.div>
-          </AnimatePresence>
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0.35 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              ease: "easeOut",
+            }}
+          >
+            <Outlet />
+          </motion.div>
         </main>
         <SiteFooter />
       </div>
